fbcfdc9bc0e71b7d0d787660e7ce0706fa56b68b
al-caveman <toraboracaveman@protonmail.com>
2020-05-15 17:15:18 +0400

    k

--- sim.py
+++ sim.py
@@ -1,6 +1,19 @@
+peeps = []
+projs = []
+
 class Person:
     def __init__(self, wallet, storage_tendency, investment_success_rate):
         self.wallet = wallet 
         self.storage_tendency = storage_tendency
         self.investment_success_rate = investment_success_rate
 
+    def invest(proj)
+        pass
+
+class Project:
+    def __init__(self, investor, cost):
+        self.investor = investor
+        self.cost = cost
+
+    def invest(proj)
+        pass

8f4d35d7179e69c60c89be831028032c46d23f9f
al-caveman <toraboracaveman@protonmail.com>
2020-05-15 17:09:12 +0400

    k

---
+++ sim.py
@@ -0,0 +1,6 @@
+class Person:
+    def __init__(self, wallet, storage_tendency, investment_success_rate):
+        self.wallet = wallet 
+        self.storage_tendency = storage_tendency
+        self.investment_success_rate = investment_success_rate
+
